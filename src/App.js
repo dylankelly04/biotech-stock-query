3840140ae4b7c0bc1b6f50f48f8a9a84c8249042
Dylan Kelly <81248183+dylankelly04@users.noreply.github.com>
2024-02-10 00:41:24 -0500

    Sample Tremor Graph

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
 import logo from "./logo.svg";
-import { LineChartHero } from "./sampleGraph";
+import { LineChart } from "@tremor/react";
+
+const chartdata = [
+  { date: "Jan 22", SemiAnalysis: 2890, "The Pragmatic Engineer": 2338 },
+  { date: "Feb 22", SemiAnalysis: 2756, "The Pragmatic Engineer": 2103 },
+  { date: "Mar 22", SemiAnalysis: 3322, "The Pragmatic Engineer": 2194 },
+  { date: "Apr 22", SemiAnalysis: 3470, "The Pragmatic Engineer": 2108 },
+  { date: "May 22", SemiAnalysis: 3475, "The Pragmatic Engineer": 1812 },
+  { date: "Jun 22", SemiAnalysis: 3129, "The Pragmatic Engineer": 1726 },
+  { date: "Jul 22", SemiAnalysis: 3490, "The Pragmatic Engineer": 1982 },
+  { date: "Aug 22", SemiAnalysis: 2903, "The Pragmatic Engineer": 2012 },
+  { date: "Sep 22", SemiAnalysis: 2643, "The Pragmatic Engineer": 2342 },
+  { date: "Oct 22", SemiAnalysis: 2837, "The Pragmatic Engineer": 2473 },
+  { date: "Nov 22", SemiAnalysis: 2954, "The Pragmatic Engineer": 3848 },
+  { date: "Dec 22", SemiAnalysis: 3239, "The Pragmatic Engineer": 3736 },
+];
+const valueFormatter = function (number) {
+  return "$ " + new Intl.NumberFormat("us").format(number).toString();
+};
 
 function App() {
   return (
@@ -29,7 +47,20 @@ function App() {
         </a> */}
       {/* </header> */}
       <div className="grid grid-cols-3 gap-4">
-        <div className="col-span-2">{LineChartHero}</div>
+        <div className="col-span-2">
+          <h3 className="text-lg font-medium text-tremor-content-strong dark:text-dark-tremor-content-strong">
+            Newsletter revenue over time (USD)
+          </h3>{" "}
+          <LineChart
+            className="mt-4 h-72"
+            data={chartdata}
+            index="date"
+            yAxisWidth={65}
+            categories={["SemiAnalysis", "The Pragmatic Engineer"]}
+            colors={["indigo", "cyan"]}
+            valueFormatter={valueFormatter}
+          />
+        </div>
         <div>
           <h1 className="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl">
             Bring{" "}
