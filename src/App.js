bf5dcb463e0d9af9c28dbf6a4a7e176fa982d5bd
Dylan Kelly <81248183+dylankelly04@users.noreply.github.com>
2024-02-10 00:23:50 -0500

    Created gridview

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import logo from "./logo.svg";
+import { LineChartHero } from "./sampleGraph";
 
 function App() {
   return (
     <div>
-      <header class="flex justify-center items-center mt-40">
-        <h1 className="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl">
-          Bring{" "}
-          <span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">
-            Transparency
-          </span>{" "}
-          To {""}
-          <span className="text-transparent bg-clip-text bg-gradient-to-r to-sky-400 from-emerald-600">
-            Biotech
-          </span>{" "}
-        </h1>
-        {/* <img src={logo} className="App-logo" alt="logo" />
+      {/* <header class="flex justify-right items-center mt-10">
+      <h1 className="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl">
+        Bring{" "}
+        <span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">
+          Transparency
+        </span>{" "}
+        To {""}
+        <span className="text-transparent bg-clip-text bg-gradient-to-r to-sky-400 from-emerald-600">
+          Biotech
+        </span>{" "}
+      </h1> */}
+      {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -26,7 +27,24 @@ function App() {
         >
           Learn React! (Test for deployment)
         </a> */}
-      </header>
+      {/* </header> */}
+      <div className="grid grid-cols-3 gap-4">
+        <div className="col-span-2">{LineChartHero}</div>
+        <div>
+          <h1 className="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl">
+            Bring{" "}
+            <span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">
+              Transparency
+            </span>{" "}
+            To {""}
+            <span className="text-transparent bg-clip-text bg-gradient-to-r to-sky-400 from-emerald-600">
+              Biotech
+            </span>{" "}
+          </h1>
+        </div>
+        <div>3</div>
+        <div>4</div>
+      </div>
     </div>
   );
 }
