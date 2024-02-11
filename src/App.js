4a43af270f8319ece59f9240b617ee8ad60d4b50
Dylan Kelly <81248183+dylankelly04@users.noreply.github.com>
2024-02-10 23:29:04 -0500

    First Commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { LineChart } from "@tremor/react";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import "flowbite";
+import { TypeAnimation } from "react-type-animation";
 
 const valueFormatter = function (number) {
   return "$ " + new Intl.NumberFormat("us").format(number).toString();
@@ -18,28 +19,19 @@ function formatDate(dateObj) {
 }
 
 function App() {
-  const [stockName, setStockName] = useState("HACK");
-  const [chartData, setChartData] = useState([
-    { date: "Jan 22", [stockName]: 2890 },
-    { date: "Feb 22", [stockName]: 2756 },
-    { date: "Mar 22", [stockName]: 3322 },
-    { date: "Apr 22", [stockName]: 3470 },
-    { date: "May 22", [stockName]: 3475 },
-    { date: "Jun 22", [stockName]: 3129 },
-    { date: "Jul 22", [stockName]: 3490 },
-    { date: "Aug 22", [stockName]: 2903 },
-    { date: "Sep 22", [stockName]: 2643 },
-    { date: "Oct 22", [stockName]: 2837 },
-    { date: "Nov 22", [stockName]: 2954 },
-    { date: "Dec 22", [stockName]: 3239 },
-  ]);
+  const [shownStocks, setShownStocks] = useState([]);
+  const [stockName, setStockName] = useState("$ORGO");
+  const [chartData, setChartData] = useState([]);
 
   const [inputValue, setInputValue] = useState({
     ticker: "ORGO",
     query: "Why is the stock going up?",
   });
   const [selectedItem, setSelectedItem] = useState("1mo");
-  const [gptResponse, setGptResponse] = useState(["GPT Response Here"]);
+  const [gptResponse, setGptResponse] = useState([
+    "GPT response here (may take 10-15 seconds to load)",
+  ]);
+  const [isLoading, setIsLoading] = useState(false);
   const [similarStocks, setSimilarStocks] = useState([]);
 
   const handleChange = (e) => {
@@ -56,13 +48,15 @@ function App() {
   };
 
   async function fetchGPTResponse(e) {
-    console.log("fetch gpt " + JSON.stringify(inputValue));
-    var req = await axios.get(
-      `https://biotech-stock-query-backend.onrender.com/api/${inputValue.ticker}?query=${inputValue.query}`
-    );
-    var data = req.data;
-    console.log(data);
-    setGptResponse(data.messages);
+    setIsLoading(true);
+    // console.log("fetch gpt " + JSON.stringify(inputValue));
+    // var req = await axios.get(
+    //   `https://biotech-stock-query-backend.onrender.com/api/${inputValue.ticker}?query=${inputValue.query}`
+    // );
+    // var data = req.data;
+    // console.log(data);
+    // setGptResponse(data.messages);
+    setIsLoading(false);
   }
 
   async function fetchSimilarStocks(e) {
@@ -71,54 +65,51 @@ function App() {
       `https://biotech-stock-query-backend.onrender.com/api/similar/${inputValue.ticker}`
     );
     var data = req.data;
-    console.log(data);
     setSimilarStocks(data.similar);
   }
 
   async function fetchData(e) {
     e.preventDefault();
-    setStockName(inputValue.ticker);
+    setStockName("$" + inputValue.ticker);
     fetchGPTResponse(e);
   }
 
   useEffect(() => {
-    const fetchDataAsync = async () => {
+    const fetchDataAsync = async (ticker) => {
       if (!stockName) return;
-
-      console.log(stockName);
 
       try {
         var response = await axios.get(
-          `https://biotech-stock-query-backend.onrender.com/data?ticker=${stockName}&time=${selectedItem}`
+          `https://biotech-stock-query-backend.onrender.com/data?ticker=${ticker.slice(
+            1
+          )}&time=${selectedItem}`
         );
 
         var data = response.data;
         data = JSON.parse(data);
         var close = data.Close;
-        console.log(close);
         var newChart = [];
 
         for (const [key, value] of Object.entries(close)) {
           var formattedDate = new Date(parseInt(key));
           newChart.push({
             date: formatDate(formattedDate),
-            [stockName]: value,
+            [ticker]: value,
           });
         }
-
         setChartData(newChart);
       } catch (error) {
         console.error("There was an error fetching the data: ", error);
       }
     };
 
-    fetchDataAsync();
+    fetchDataAsync(stockName);
     fetchSimilarStocks();
   }, [stockName, selectedItem]); // Effect runs only when stockName changes
 
   return (
     <div className="grid grid-cols-3 grid-rows-5 gap-5 h-full p-5">
-      <div className="col-span-2 row-span-4 border border-slate-100 rounded-md bg-[#403e3e] p-5">
+      <div className="col-span-2 row-span-4 border border-slate-100 rounded-md bg-zinc-800 p-5">
         <h3 className="text-lg font-medium text-white">
           Stock Price Trend (USD)
         </h3>
@@ -134,14 +125,15 @@ function App() {
         />
       </div>
       <div className="row-span-2">
+        <h1 className="text-6xl font-extrabold text-cyan-300">Equity^2</h1>
         <h1 className="mb-4 text-3xl font-extrabold text-white md:text-5xl lg:text-6xl mt-1 mr-2">
           We're Bringing{" "}
           <span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">
-            Transparency
+            Equity
           </span>{" "}
           To {""}
           <span className="text-transparent bg-clip-text bg-gradient-to-r to-sky-400 from-emerald-600">
-            Biotech
+            Equities
           </span>{" "}
         </h1>
         {/* <form class="max-w-sm mx-auto"> */}
@@ -282,40 +274,53 @@ function App() {
             </div>
           </button>
           <div>
-            <label
-              for="terms"
-              class="text-sm font-medium text-gray-900 dark:text-gray-500"
-            >
+            <label for="terms" class="text-sm font-medium text-gray-500">
               This is not financial advice and may contain mistakes.
             </label>
           </div>
         </form>
       </div>
-      <div class="row-span-3 overflow-y-auto bg-gray-200 mt-5 p-4">
-        {gptResponse.map((message) => {
-          return (
-            <div class="mb-4">
-              <div class="text-sm font-medium text-gray-900 dark:text-white">
-                {message}
-              </div>
-            </div>
-          );
-        })}
+      <div class="row-span-3 overflow-y-auto bg-black p-6 border rounded-md border-gray-950">
+        {isLoading ? (
+          <span class="text-xs font-medium text-cyan-400 dark:text-white font-mono">
+            Loading
+            <TypeAnimation sequence={["..."]} speed={20} />
+          </span>
+        ) : (
+          gptResponse
+            .filter((r) => r.length !== 0)
+            .map((message) => {
+              return (
+                <div class="mb-4">
+                  <div class="text-xs font-medium text-cyan-400 dark:text-white font-mono">
+                    <TypeAnimation
+                      sequence={message}
+                      wrapper="span"
+                      speed={70}
+                      repeat={1}
+                      cursor={false}
+                    />
+                    <br />
+                  </div>
+                </div>
+              );
+            })
+        )}
       </div>
       <div className="border rounded-md border-sky-700">
         <h3 className="text-slate-200 text-center">Compare Similar Stocks</h3>
-        <div className="border rounded-md border-slate-800 bg-yellow-700">
-          Stocks
-        </div>
-        <div>
+        <div className="grid grid-flow-dense grid-cols-3 p-5 gap-3">
           {similarStocks.map((stock) => {
             return (
-              <a
-                href={`https://finance.yahoo.com/quote/${stock}`}
-                className="inline-block px-1"
+              <button
+                onClick={(e) => {
+                  const ticker = e.target.textContent;
+                  setShownStocks([...shownStocks, ticker]);
+                }}
+                className="inline-block px-1 bg-zinc-400 rounded-lg text-center py-1 hover:bg-zinc-500 duration-200"
               >
-                {stock}
-              </a>
+                ${stock}
+              </button>
             );
           })}
         </div>
